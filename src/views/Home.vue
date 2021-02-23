519268ac449c1afe91f47daa0ce33233278205f0
William0832 <squall0832@gmail.com>
2021-02-24 05:15:51 +0800

    fetch all data as mounted in Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,8 +7,19 @@
 
 <script>
 import asideMenu from '../components/asideMenu'
+import { mapActions } from 'vuex'
+import { types } from '../store/types'
+
 export default {
   name: 'Home',
-  components: { asideMenu }
+  components: { asideMenu },
+  methods: {
+    ...mapActions([types.FETCH_LOCATION, types.FETCH_STORES])
+
+  },
+  mounted () {
+    this.FETCH_LOCATION().then(e => console.log(e))
+    this.FETCH_STORES().then(e => console.log(e))
+  }
 }
 </script>
