2c229b81738351f40ce688a6ae9498f6f5c505e9
William0832 <squall0832@gmail.com>
2021-02-23 18:24:52 +0800

    add css links and add Home, NotFound page

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import Notfound from '../views/NotFound.vue'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  }, {
+    path: '/:pathMatch(.*)*',
+    name: 'Not-found',
+    component: Notfound
   }
 ]
 
---
+++ src/views/NotFound.vue
@@ -0,0 +1,5 @@
+<template lang="pug">
+.wrap
+  h1 404 Not found
+  router-link(to='/') Back to Home
+</template>
