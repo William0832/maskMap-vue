689ca96d4d3a1a65854d11945d241e086de52484
William0832 <squall0832@gmail.com>
2021-02-24 01:56:46 +0800

    add asideMenu component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,66 +1,14 @@
 <template lang='pug'>
 #home
-  .aside-menu
-
-    .wraps
-      label 縣市:
-        select
-          option 台北市
-      label 行政區:
-        select
-          option 北投區
-
-    .wraps
-      label
-        i.fas.fa-search-location
-        span 關鍵字搜尋:
-        input(type='text', placeholder='請輸入關鍵字')
-
-    ul.store-list
-      li.store-info.wraps
-        h1 XX藥局
-        .mask-info
-          i.fas.fa-user
-          span 大人口罩: 500個
-        .mask-info
-          i.fas.fa-baby
-          span 小孩小孩口罩: 500個
-        .mask-info 最後更新時間:
-        button.btn-store-detail
-          i.fas.fa-info-circle
-          | 看詳細資訊
-
-      li.store-info.wraps
-        h1 XX藥局
-        .mask-info
-          i.fas.fa-user
-          span 大人口罩: 500個
-        .mask-info
-          i.fas.fa-baby
-          span 小孩小孩口罩: 500個
-        .mask-info 最後更新時間:
-        button.btn-store-detail
-          i.fas.fa-info-circle
-          | 看詳細資訊
-
-      li.store-info.wraps
-        h1 XX藥局
-        .mask-info
-          i.fas.fa-user
-          span 大人口罩: 500個
-        .mask-info
-          i.fas.fa-baby
-          span 小孩小孩口罩: 500個
-        .mask-info 最後更新時間:
-        button.btn-store-detail
-          i.fas.fa-info-circle
-          | 看詳細資訊
+  asideMenu
 
   #mask-map
 </template>
 
 <script>
+import asideMenu from '../components/asideMenu'
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: { asideMenu }
 }
 </script>
